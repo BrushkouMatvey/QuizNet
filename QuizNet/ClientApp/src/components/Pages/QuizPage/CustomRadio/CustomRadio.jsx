58f7f2bcd72754e6db9abc33e36181a1747b65a8
BrushkouMatvey <brushkoumatvey@gmail.com>
2020-04-01 07:23:20 +0300

    .

---
+++ QuizNet/ClientApp/src/components/Pages/QuizPage/CustomRadio/CustomRadio.jsx
@@ -0,0 +1,70 @@
+﻿import React from 'react';
+import s from "./CustomRadio.module.css";
+import { NavLink } from "react-router-dom";
+
+
+// export default class RadioButton extends React.Component = (props) => {
+//     return (
+
+//         <NavLink to={props.link}><button className={s.button}> {props.text}</button></NavLink>
+
+//     );
+// };
+
+
+
+
+class RadioBtn extends React.Component{
+
+    constructor(props) {
+        super(props);
+    }
+
+    handleClick(){
+        this.props.handler(this.props.index);
+    }
+
+    render() {
+        return (
+            <div className="radio-btn-group" onClick={this.handleClick.bind(this)}>
+                <div className={this.props.isChecked ? "radiobtn checked" : "radiobtn unchecked"} data-value={this.props.value}></div>
+                <label>{this.props.text}</label>
+            </div>
+        );
+    }
+}
+
+class RadioGrp extends React.Component{
+
+    constructor() {
+        super();
+        this.state = {
+          selectedIndex: null,
+          selectedValue: null,
+          options: ["option 0","option 1","option 2","option 3"]
+        };
+    }
+
+    toggleRadioBtn(index){
+        this.setState({
+          selectedIndex: index,
+          selectedValue: this.state.options[index],
+          options: this.state.options
+        });
+    }
+
+    render() {
+
+        const { options } = this.state;
+
+        const allOptions = options.map((option, i) => {
+            return <RadioBtn key={i} isChecked={(this.state.selectedIndex == i)} text={option} value={option} index={i} handler={this.toggleRadioBtn.bind(this)} />
+        });
+
+        return (
+            <div>{allOptions}</div>
+        );
+    }
+}
+
+export default RadioGrp;
---
+++ QuizNet/ClientApp/src/components/Pages/QuizPage/QuizPage.jsx
@@ -0,0 +1,55 @@
+﻿import * as React from 'react';
+
+import { Button } from '../Button/Button';
+import s from './QuizPage.module.css'
+
+
+
+
+
+class QuizPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: "",
+            questionName: "",
+            answers: [],
+            rightAnswer: ""
+        };
+
+    }
+
+    loadData() {
+        var xhr = new XMLHttpRequest();
+        xhr.open("get", this.props.apiUrl, true);
+        xhr.onload = (() => {
+            var data = JSON.parse(xhr.responseText);
+            this.setState({ id: data.id, questionName: data.questionName, answers: data.answers, rightAnswer: data.rightAnswer });
+        }).bind(this);
+        xhr.send();
+    }
+    componentDidMount() {
+        this.loadData();
+    }
+
+    render() {
+        return <div className={s.quizTopicsPage}>
+            <div>{this.state.id} {this.state.questionName}</div>
+            <div>{this.state.answers[0]}</div>
+            <div>{this.state.answers[1]}</div>
+            <div>{this.state.answers[2]}</div>
+            <div>{this.state.answers[3]}</div>
+            <button>Next</button>
+
+
+            {/* <div>1 Вопрос 1 ывлоравылоарыволарывлоралывраылдрмдывщгарфдшырмфыдварылварфдлыварфлдывоа</div>
+            <div></div>
+            <div>ответ 2</div>
+            <div>ответ 3</div>
+            <div>ответ 4</div>
+            <button>next</button> */}
+        </div>
+    }
+}
+
+export default QuizPage;
