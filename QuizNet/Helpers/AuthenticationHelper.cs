e9b0324ca4bdeff7e3e281465ae23273d9db5e4e
Oleg Zhukovsky 750501 <lecongjung@gmail.com>
2020-04-16 08:06:30 +0300

    Added identity

---
+++ QuizNet/Helpers/AuthenticationHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using QuizNet.Models.Identity;
+
+namespace QuizNet.Helpers
+{
+    public static class AuthenticationHelper
+    {
+        public static string GenerateJwtToken(string email, User user, IConfiguration configuration)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString())
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtKey"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var expires = DateTime.Now.AddDays(Convert.ToDouble(configuration["JwtExpireDays"]));
+
+            var token = new JwtSecurityToken(
+                configuration["JwtIssuer"],
+                configuration["JwtIssuer"],
+                claims,
+                expires: expires,
+                signingCredentials: creds
+            );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ QuizNet/Models/Identity/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AspNetCore.Identity.Mongo.Model;
+
+namespace QuizNet.Models.Identity
+{
+    //Add any custom field for a user
+    public class User : MongoUser
+    {
+        public string Name { get; set; }
+
+        //public string LastName { get; set; }
+
+        //public DateTime Birthdate { get; set; }
+
+        //public string Country { get; set; }
+
+        //public string State { get; set; }
+
+        //public string City { get; set; }
+    }
+}
